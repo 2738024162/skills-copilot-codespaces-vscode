b78aadb8eeab6bf3d698f93ef10b8d58d8b74760
健康的牛奶土司 <148662874+2738024162@users.noreply.github.com>
2025-01-05 12:00:00 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,2 +1,27 @@
 // create web server 
-// create a route for /comments
+const express = require('express');
+const app = express();
+// parse request body
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+// allow cross-origin requests
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  next();
+});
+// create in-memory database
+const comments = [];
+// create a new comment
+app.post('/comments', (req, res) => {
+  const comment = req.body;
+  comments.push(comment);
+  res.status(201).json(comment);
+});
+// list all comments
+app.get('/comments', (req, res) => {
+  res.json(comments);
+});
+// start the web server
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
